774e1380ada3fdc5d47f89b464dee1297f1e2845
NguyenDev15 <nguyenn13122005@gmail.com>
2024-08-17 18:03:03 +0700

    Start Java

---
+++ src/learning_java/Learning_Java.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package learning_java;
+
+/**
+ *
+ * @author hnguynis.no1
+ */
+public class Learning_Java {
+
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+    }
+    
+}
---
+++ src/learning_java/Lesson_1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package learning_java;
+
+/**
+ *
+ * @author hnguynis.no1
+ */
+public class Lesson_1 {
+    
+}
