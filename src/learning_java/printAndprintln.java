42120c12b6349ab6cd39fd519f758923634d9f90
NguyenDev15 <nguyenn13122005@gmail.com>
2024-08-18 14:23:53 +0700

    Print And Println

---
+++ src/learning_java/printAndprintln.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package learning_java;
+
+/**
+ *
+ * @author hnguynis.no1
+ */
+public class printAndprintln {
+
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        // println
+        System.out.println("Hello word");
+        System.out.println("Hello word");
+        System.out.println("Hello word");
+        // print
+        System.out.print("Hello guys");
+        System.out.print("Hello guys");
+        System.out.print("Hello guys");
+        // \t 
+        System.out.print("\nCộng Hòa Xã Hội Chủ Nghĩa Việt Nam");
+        // \n
+        System.out.print("\n\tĐộc lập - Tự do -  Hạnh phúc\n");
+        
+        // sử dụng nháy kép
+        System.out.println("He said: \"I Miss You\" ");
+    }
+    
+}
