e55b745b13e1d655851678a8be8f037d4f4236de
NguyenDev15 <nguyenn13122005@gmail.com>
2024-08-18 16:11:38 +0700

    Data Type Coercion

---
+++ src/learning_java/DataTypeCoercion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package learning_java;
+
+/**
+ *
+ * @author hnguynis.no1
+ */
+public class DataTypeCoercion {
+    public static void main(String[] args) {
+        // Ép kiểu dữ liệu
+        
+        int a = 5;
+        int b = 10;
+        double kq = (double)a/b; // ép kiểu int sang double
+        System.out.println(kq);
+        
+        // nên ép từ kiểu dữ liệu nhỏ sang lớn để tránh bị sai 
+    }
+}
