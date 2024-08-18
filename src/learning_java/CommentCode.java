50232f0393b4ec599d13aee9561c3f9264700b8d
NguyenDev15 <nguyenn13122005@gmail.com>
2024-08-18 15:30:01 +0700

    Comment Code

---
+++ src/learning_java/CommentCode.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package learning_java;
+
+/**
+ *
+ * @author hnguynis.no1
+ */
+public class CommentCode {
+    public static void main(String[] args) {
+        // Đây là ghi chú 1 dòng
+//        System.out.println("hello");
+//        System.out.println("hello");
+        System.out.println("hello");
+        
+        /*
+        Đây là ghi chú
+        nhiều dòng
+        dòng 1 
+        dòng 2
+        */
+    }
+}
