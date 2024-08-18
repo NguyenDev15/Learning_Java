1d6f8f17db7272b3201a48460f33591c655fa0f3
NguyenDev15 <nguyenn13122005@gmail.com>
2024-08-18 15:39:38 +0700

    Data Type

---
+++ src/learning_java/DataType.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package learning_java;
+
+/**
+ *
+ * @author hnguynis.no1
+ */
+public class DataType {
+    public static void main(String[] args) {
+        // Kiểm tra kiểu Int
+        System.out.println("Max value kiểu int: " + Integer.MAX_VALUE);
+        System.out.println("Min value kiểu int: " + Integer.MIN_VALUE);
+        // Kiểm tra kiểu byte
+        System.out.println("Max value kiểu byte: " + Byte.MAX_VALUE);
+        System.out.println("Min value kiểu byte: " + Byte.MIN_VALUE);
+        
+        // và còn nhiều các kiểu dữ liệu các như long, double, short,....
+    }
+}
